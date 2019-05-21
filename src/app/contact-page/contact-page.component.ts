94df3538ca60cd7891a9877e324cbd63ad6134bc
Dhitivej Chuersuwan <d_chuersuwan@hotmail.com>
2019-05-21 18:00:03 +0700

    Finished Starter App

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
+  messageForm: FormGroup;
+  submitted: Boolean = false;
+  sucess: Boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    this.submitted = true;
 
-  ngOnInit() {
+    if (this.messageForm.invalid) {
+      return;
+    }
+
+    this.sucess = true;
   }
 
+  ngOnInit() {}
 }
